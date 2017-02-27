02140d290cc7407a1d51c8f56341e198bab793dd
jcarlos <carlosmaster10@gmail.com>
2017-02-27 12:43:49 -0300

    first commit. PropertiesManager added

---
+++ src/main/java/br/com/commons/util/PropertiesManager.java
@@ -0,0 +1,30 @@
+package br.com.commons.util;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class PropertiesManager {
+
+	private static Properties properties;
+	// TODO change to general.config later
+	private static String path = "general.test.config";
+
+	public static String getProperty(String propertie) {
+		try {
+			if (properties == null) {
+				properties = new Properties();
+				FileInputStream file = new FileInputStream(path);
+				properties.load(file);
+			}
+		} catch (Exception e) {
+			System.out.println("Não foi possível carregar arquivo de propeties");
+		}
+
+		return properties.getProperty(propertie);
+	}
+
+	public static void setNewPath(String newpath) {
+		path = newpath;
+		properties = null;
+	}
+}
